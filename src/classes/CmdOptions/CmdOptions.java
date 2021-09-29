f9cab5b7f518c211f9aa4044b7ee987041c571a4
Pisklov Vadim <pisklovv@Hexagontech.co.il>
2021-09-29 15:05:36 +0300

    init

--- src/classes/CmdOptions/CmdOptions.java
+++ src/classes/CmdOptions/CmdOptions.java
@@ -29,15 +29,16 @@ public class CmdOptions {
                 .desc("Show order and related product data");
 
 //        options.addOption(CmdCommands.CREATE_ORDER.command, false, "Test create order");
+//        options.addOption(CmdCommands.UPDATE_PRODUCT.command, "update-product",false, "Update product");
         options.addOption(CmdCommands.SHOW_PRODUCTS.command, "show-products",false, "Show products");
         options.addOption(CmdCommands.CREATE_PRODUCT.command, "create-product",false, "Create product");
-        options.addOption(CmdCommands.UPDATE_PRODUCT.command, "update-product",false, "Update product");
         options.addOption(CmdCommands.DELETE_ALL_PRODUCTS.command, "delete-products",false, "Delete all product");
-        options.addOption(deleteProductOption.build());
         options.addOption(CmdCommands.CREATE_ORDER.command, "create-order",false, "Create order");
         options.addOption(CmdCommands.SHOW_ORDERED_PRODUCTS.command, "show-ordered-products",false, "Show ordered products");
-        options.addOption(showOrderOption.build());
         options.addOption(CmdCommands.LIST_ORDERS.command, "list-orders",false, "List orders");
+
+        options.addOption(deleteProductOption.build());
+        options.addOption(showOrderOption.build());
 
     }
 
